48a295e08a399b427f110d5aeade9829af90f764
Noxety <thura3.shine33@gmail.com>
2025-04-04 12:14:01 +0630

    added classes

---
+++ ADI_Assignment/Classes/Client_Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freelancer_client.Classes
+{
+    public class Client_Project
+    {
+        private int id;
+        private int client_id;
+        private int project_id;
+
+        public int Id { get => id; set => id = value; }
+        public int Client_id { get => client_id; set => client_id = value; }
+        public int Project_id { get => project_id; set => project_id = value; }
+
+    }
+}
---
+++ ADI_Assignment/Classes/Freelancer_Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freelancer_client.Classes
+{
+    public class Freelancer_Project
+    {
+        private int id;
+        private int freelancer_id;
+        private int project_id;
+        private DateTime applied_date;
+        public int Id { get => id; set => id = value; }
+        public int Freelancer_id { get => freelancer_id; set => freelancer_id = value; }
+        public int Project_id { get => project_id; set => project_id = value; }
+        public DateTime Applied_date { get => applied_date; set => applied_date = value; }
+    }
+}
